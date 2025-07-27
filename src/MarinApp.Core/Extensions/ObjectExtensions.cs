20a8a0bb14b4da081c0b5bd8959f7734a2385009
Oscar Marin <oscar@marin.cr>
2025-07-26 20:32:56 -0500

    Add ObjectExtensions with ToJson serialization method
    Introduces a static extension method ToJson for serializing objects to indented JSON, handling reference cycles and exceptions. Throws ArgumentNullException for null input and wraps serialization errors in InvalidOperationException.

---
+++ src/MarinApp.Core/Extensions/ObjectExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+namespace MarinApp.Core.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static string ToJson(this object obj)
+        {
+            if (obj == null) throw new ArgumentNullException(nameof(obj));
+            try
+            {
+                var options = new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                    ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles
+                };
+                return JsonSerializer.Serialize(obj, obj.GetType(), options);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Failed to serialize object to JSON.", ex);
+            }
+        }
+    }
+}
