c19e423bc5f9bc59d0f91d301e2c76626ea42e14
Oscar Marin <oscar@marin.cr>
2025-08-11 20:52:26 -0500

    Update ConfigurationExtensions.cs

--- MarinApp.Expenses/ConfigurationExtensions.cs
+++ MarinApp.Expenses/ConfigurationExtensions.cs
@@ -1,11 +1,6 @@
 ﻿using MarinApp.Expenses.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MarinApp.Expenses
 {
