832b8eb9cdda65239aabfb05d957c37108eecc60
Oscar Marin <oscar@marin.cr>
2025-08-11 20:51:52 -0500

    Add ConfigurationExtensions for service registration
    Introduces ConfigurationExtensions.cs with an extension method to register ExpenseDbContext and ExpenseService in the service collection, simplifying dependency injection setup for MarinApp Expenses.

---
+++ MarinApp.Expenses/ConfigurationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using MarinApp.Expenses.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarinApp.Expenses
+{
+    public static class ConfigurationExtensions
+    {
+        /// <summary>
+        /// Adds the MarinApp Expenses services to the specified service collection.
+        /// </summary>
+        /// <param name="services">The service collection to add the services to.</param>
+        /// <returns>The updated service collection.</returns>
+        public static IServiceCollection AddMarinAppExpenses(this IServiceCollection services, string connectionString)
+        {
+            // Register the DbContext factory for ExpenseDbContext
+            services.AddDbContextFactory<ExpenseDbContext>(options =>
+            {
+                options.UseNpgsql(connectionString);
+            });
+            // Register the ExpenseService
+            services.AddScoped<ExpenseService>();
+            return services;
+        }
+    }
+}
