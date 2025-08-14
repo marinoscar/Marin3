f9320f72a6a2f27c0b636474be07e6de32fb75dd
Oscar Marin <oscar@marin.cr>
2025-08-13 19:28:10 -0500

    Remove CodeBuilder and Core.Components, add DemoAgent project
    Deleted MarinApp.CodeBuilder and MarinApp.Core.Components projects and their files. Added new MarinApp.DemoAgent project with initial RequirementsAgent class. Moved MarinApp.Expenses project and its files into the src directory. Updated Marin3.sln to reflect these changes.

---
+++ src/MarinApp.DemoAgent/RequirementsAgent.cs
@@ -0,0 +1,13 @@
+﻿using AutoGen;
+
+namespace MarinApp.DemoAgent
+{
+    public class RequirementsAgent(
+    AgentId id,
+    IAgentRuntime runtime,
+    ) :
+        BaseAgent(id, runtime, "MyAgent", null),
+    {
+
+    }
+}
