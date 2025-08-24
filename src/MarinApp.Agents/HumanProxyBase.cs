79eb9a3f401f496e9bc9bf21d6fa304ef1df00c9
Oscar Marin <oscar@marin.cr>
2025-08-24 10:00:26 -0500

    Add abstract HumanProxyBase class
    Introduces HumanProxyBase as an abstract class inheriting from AgentBase. This class provides a foundation for human proxy agents, requiring agent history and logger services in its constructor.

---
+++ src/MarinApp.Agents/HumanProxyBase.cs
@@ -0,0 +1,17 @@
+﻿using MarinApp.Agents.Data;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarinApp.Agents
+{
+    public abstract class HumanProxyBase : AgentBase
+    {
+        protected HumanProxyBase(IAgentHistoryService agentHistoryService, ILoggerFactory loggerFactory) : base(agentHistoryService, loggerFactory)
+        {
+        }
+    }
+}
