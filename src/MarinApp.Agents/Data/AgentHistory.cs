d70b0151bfbfc157c0ea154e25d16b348c4bea3c
Oscar Marin <oscar@marin.cr>
2025-08-25 15:58:44 -0500

    Add AgentHistory and AgentItem data models
    Introduces AgentHistory as a list of AgentItem with an associated ChatHistory, and AgentItem to encapsulate message content and metadata for agent interactions. These models will support structured handling of agent chat history and messages.

---
+++ src/MarinApp.Agents/Data/AgentHistory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarinApp.Agents.Data
+{
+    public class AgentHistory : List<AgentItem>
+    {
+        public ChatHistory ChatHistory { get; set; } = new();
+    }
+}
---
+++ src/MarinApp.Agents/Data/AgentItem.cs
@@ -0,0 +1,17 @@
+﻿using OpenAI.Chat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarinApp.Agents.Data
+{
+    public class AgentItem
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public ChatMessageContent Content { get; set; } = default!;
+
+        public AgentMessage AgentMessage { get; set; } = default!;
+    }
+}
